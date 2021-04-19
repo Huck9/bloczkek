c890a35a91a2113474ee6379bf1cf746dc8cc802
kppic <kppichlinski@gmail.com>
2021-04-19 22:19:14 +0200

    added 3 panels (admin and worker, and empty user_panel)

---
+++ admin_panel.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php include('head.php'); ?>
+
+<body>
+
+<div>
+
+    <div class="content">
+
+        <?php include('header.php'); ?>
+
+        <button>Wyloguj</button>
+
+        <div>
+
+            <a href = 'register.php'><button onclick="">Dodaj pracownika</button></a>
+
+        </div>
+
+    </div>
+
+</div>
+
+</body>
+
+
+
+<?php
+
