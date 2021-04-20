3a4eaab73d467cfe6a7566f1c77534ba59de282c
Piotr <31008706+halucyn0@users.noreply.github.com>
2021-04-20 02:27:39 +0200

    Nowy plik sql. Dodane pole salt oraz zmiana typu hasła na varchar(255).

--- bloczkek.sql
+++ bloczkek.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Czas generowania: 13 Kwi 2021, 19:31
+-- Czas generowania: 20 Kwi 2021, 02:26
 -- Wersja serwera: 10.4.18-MariaDB
--- Wersja PHP: 7.4.16
+-- Wersja PHP: 8.0.3
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,27 +28,23 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `users` (
-  `ID` int(16) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `surname` varchar(50) NOT NULL,
-  `login` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `accountType` varchar(50) NOT NULL
+                         `ID` int(16) NOT NULL,
+                         `name` varchar(50) NOT NULL,
+                         `surname` varchar(50) NOT NULL,
+                         `login` varchar(50) NOT NULL,
+                         `email` varchar(50) NOT NULL,
+                         `password` varchar(255) NOT NULL,
+                         `salt` varchar(10) NOT NULL,
+                         `accountType` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Zrzut danych tabeli `users`
 --
 
-INSERT INTO `users` (`ID`, `name`, `surname`, `login`, `email`, `password`, `accountType`) VALUES
-(1, 'ADMIN', 'ADMIN', 'ADMIN', 'ADMIN', '73acd9a5972130b75066c82595a1fae3', 'ADMIN'),
-(2, 'Rossmann', 'XDDD', 'alfred90', 'siema@elo.pl', '8ee22e2997a001aaf96cbf5d35a8b8e4', 'user'),
-(3, 'asdasfas', 'fas', 'asdfasdf', 'siema@elo.pl', '26c651755d2aba183c4fde5f0b17bd66', 'user'),
-(4, 'dgsgsdg', 'sdfsdfsd', 'asdasd', 'siema@elo.pl', '813accfbec3d393e659d47f2a4fb4b3d', 'user'),
-(5, 'XDD', 'XDD', 'XDD', 'XDD', 'XDD', 'XDD'),
-(6, 'asdasfs', 'asjhdag', 'ksajdhaskd', 'siema@elo.pl', '1a06316728878fe217bb735551514549', 'user'),
-(7, 'aslkdfjald', 'laskdjald', 'dlkfjal', 'cozababa@ja.kurwie', 'e6cea7aa1f512e6da0ff0e8d72bd4323', 'user');
+INSERT INTO `users` (`ID`, `name`, `surname`, `login`, `email`, `password`, `salt`, `accountType`) VALUES
+(4, 'Stefan', 'Kobza', 'stefankobza', 'stefan@kobza.pl', '18612578641ae34bc4a20a233b1467f31154701205449a2fe760961067540197', 'WFQ6mA4Oxx', 'user'),
+(5, 'Kamil', 'Wons', 'kwons@123', 'kwons@wp.pl', '3fa4b7e8be77d099250754892fa10208fa7b99d47694c203774ee0d2fb1cc882', 'HHtQhQJRG5', 'ADMIN');
 
 --
 -- Indeksy dla zrzutów tabel
@@ -58,7 +54,7 @@ INSERT INTO `users` (`ID`, `name`, `surname`, `login`, `email`, `password`, `acc
 -- Indeksy dla tabeli `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`ID`);
+    ADD PRIMARY KEY (`ID`);
 
 --
 -- AUTO_INCREMENT dla zrzuconych tabel
@@ -68,7 +64,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT dla tabeli `users`
 --
 ALTER TABLE `users`
-  MODIFY `ID` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+    MODIFY `ID` int(16) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
