e8dfa053eb64b0a8ec148cc61709f445b314fb00
Kacper9768 <zelazowski00@gmail.com>
2021-04-26 23:30:05 +0200

    home-page

--- index.php
+++ index.php
@@ -1,103 +1,102 @@
 <!doctype html>
 <html class="no-js" lang="">
 
-<?php include('head.php'); ?>
+<head>
+<meta charset="utf-8">
+<link rel="icon"  type="image/png" href="assets/images/favicon.ico">
+<title>BloczKEK</title>
+
+<link rel="stylesheet" href="styles/home_page.css">
+
+<script  src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"  integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+      
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Raleway:100,300,400,500,700,900" rel="stylesheet">
+</head>
 
 <body>
 
-<div>
+<header class="header-area header-sticky">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <nav class="main-nav">
+                    <a href="#" class="logo">
+                        <img src="assets/images/logoWide.png" alt="BloczKEK"/>
+                    </a>
+                    <ul class="nav">
+                        <li><a href="/" class="active">Strona Główna</a></li>
+                        <li><a href="/about.php">O Aplikacji</a></li>
+                        <li><a href="/contact.php">Kontakt</a></li>
+                       
+                    </ul>
+                    <ul class="nav" id="leftNav">
+                    <li><a href="login.php" id="login">Login</a></li>
+                    </ul>
+                    <a class='menu-trigger'>
+                        <span>Menu</span>
+                    </a>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
+<section class="section home-feature">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="features-small-item">
+                                <div class="icon">
+                                    <i><img src="assets/images/icons/atom-solid.svg" alt=""></i>
+                                </div>
+                                <h5 class="features-title">Nowoczesny system</h5>
+                                <p>System bazujący na najnowszej i najlepszej technologii</p>
+                            </div>
+                        </div>
 
-    <div class="content">
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="features-small-item">
+                                <div class="icon">
+                                    <i><img src="assets/images/icons/id-card-alt-solid.svg" alt=""></i>
+                                </div>
+                                <h5 class="features-title">Szybki kontakt</h5>
+                                <p>Prosty w obsłudze komunikator dostępny całą dobe</p>
+                            </div>
+                        </div>
 
-        <?php include('header.php'); ?>
-
-        <form action="index.php" method="POST">
-
-            <div class="forms">
-
-                <div class="form-group">
-                    <p>Login: <label for="login"></label><input type="text" name="login" class="form-control" id="login"
-                                                                placeholder="..."></p>
-                </div>
-
-                <div class="form-group">
-                    <p>Hasło: <label for="password"></label><input type="password" name="password" class="form-control"
-                                                                   id="password" placeholder="..."></p>
-                </div>
-
-                <div class="userButtons">
-                    <div class="loginArea">
-                        <input type="submit" name="sign" id="sign_in" value="Zaloguj" class="btn btn-outline-primary"
-                               title="Pamiętaj o wylogowaniu się po zakończeniu pracy!">
+                        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+                            <div class="features-small-item">
+                                <div class="icon">
+                                    <i><img src="assets/images/icons/ambulance-solid.svg" alt=""></i>
+                                </div>
+                                <h5 class="features-title">Gwarancja bezpieczeństwa</h5>
+                                <p>Bezpieczny i niezawodny system dostępny całą dobe, nawet w niedzielę</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
-                <div id="error"></div>
             </div>
+        </div>
+    </section>
 
-        </form>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p class="copyright">Copyright &copy; 2021 BloczKEK</p>
+                </div>
+            </div>
+        </div>
+    </footer>
 
-    </div>
-
-</div>
-<script type="text/javascript" src="scripts.js"></script>
 </body>
 
-</html>
-
 <?php
-
-require_once("config.php");
-global $config;
-
-$pdo = new PDO($config['dsn'], $config['username'], $config['password']);
-
-if (isset($_POST['sign'])) {
-
-    $login = $_POST['login'];
-    $password = $_POST['password'];
-
-
-    $stm = $pdo->query("SELECT * FROM users WHERE login='$login'");
-    $user = $stm->fetch();
-
-
-    if ($user > 0) {
-
-
-        $salt = $user['salt'];
-        $checkPass = hash('sha256', $password . $salt);
-
-
-        if ($checkPass == $user['password']) {
-            $role = $user['accountType'];
-            session_start();
-            $_SESSION['name'] = $login;
-            $_SESSION['role'] = $role;
-            if ($role == 'user') {
-                header("location: user_panel.php");
-            } elseif ($role == 'pracownik') {
-                header("location: worker_panel.php");
-            } elseif ($role == 'ADMIN') {
-                header("location: admin_panel.php");
-            }
-        } else {
-            ?>
-            <script type="text/javascript">
-                document.getElementById("error").innerHTML = "<?php echo "Zły login lub hasło, spróbuj ponownie" ?>";
-            </script>
-            <?php
-        }
-
-    } else {
-        ?>
-        <script type="text/javascript">
-            document.getElementById("error").innerHTML = "<?php echo "Zły login lub hasło, spróbuj ponownie" ?>";
-        </script>
-        <?php
-    }
-}
-
-?>
-
 
---
+++ login.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php include('head.php'); ?>
+
+<body>
+
+<div>
+
+    <div class="content">
+
+        <?php include('header.php'); ?>
+
+        <form action="index.php" method="POST">
+
+            <div class="forms">
+
+                <div class="form-group">
+                    <p>Login: <label for="login"></label><input type="text" name="login" class="form-control" id="login"
+                                                                placeholder="..."></p>
+                </div>
+
+                <div class="form-group">
+                    <p>Hasło: <label for="password"></label><input type="password" name="password" class="form-control"
+                                                                   id="password" placeholder="..."></p>
+                </div>
+
+                <div class="userButtons">
+                    <div class="loginArea">
+                        <input type="submit" name="sign" id="sign_in" value="Zaloguj" class="btn btn-outline-primary"
+                               title="Pamiętaj o wylogowaniu się po zakończeniu pracy!">
+                    </div>
+                </div>
+
+                <div id="error"></div>
+            </div>
+
+        </form>
+
+    </div>
+
+</div>
+<script type="text/javascript" src="scripts.js"></script>
+</body>
+
+</html>
+
+<?php
+
+require_once("config.php");
+global $config;
+
+$pdo = new PDO($config['dsn'], $config['username'], $config['password']);
+
+if (isset($_POST['sign'])) {
+
+    $login = $_POST['login'];
+    $password = $_POST['password'];
+
+
+    $stm = $pdo->query("SELECT * FROM users WHERE login='$login'");
+    $user = $stm->fetch();
+
+
+    if ($user > 0) {
+
+
+        $salt = $user['salt'];
+        $checkPass = hash('sha256', $password . $salt);
+
+
+        if ($checkPass == $user['password']) {
+            $role = $user['accountType'];
+            session_start();
+            $_SESSION['name'] = $login;
+            $_SESSION['role'] = $role;
+            if ($role == 'user') {
+                header("location: user_panel.php");
+            } elseif ($role == 'pracownik') {
+                header("location: worker_panel.php");
+            } elseif ($role == 'ADMIN') {
+                header("location: admin_panel.php");
+            }
+        } else {
+            ?>
+            <script type="text/javascript">
+                document.getElementById("error").innerHTML = "<?php echo "Zły login lub hasło, spróbuj ponownie" ?>";
+            </script>
+            <?php
+        }
+
+    } else {
+        ?>
+        <script type="text/javascript">
+            document.getElementById("error").innerHTML = "<?php echo "Zły login lub hasło, spróbuj ponownie" ?>";
+        </script>
+        <?php
+    }
+}
+
+?>
+
+
